6a26fe1c8d7ef0a3497b8bc4c6355c8a7a1d76bd
Tom <tomdflock@gmail.com>
2013-11-10 22:14:10 -0700

    Played around with reel counter code
    How you work interrupts?

--- Reel Counters/Inventory Reel Counter/v1/Arduino Code/Inventory_Reel_Counter_v3/Inventory_Reel_Counter_v3.ino
+++ Reel Counters/Inventory Reel Counter/v1/Arduino Code/Inventory_Reel_Counter_v3/Inventory_Reel_Counter_v3.ino
@@ -1,35 +1,40 @@
- #define encoder0PinA 3
+// setup software serial on pins 8 (RX) and 4 (tx)
+#include <SoftwareSerial.h>
+SoftwareSerial Serial7Segment(8, 4); //RX pin, TX pin
 
-#define encoder0PinB 2
+// setup pin configuration
+const char encoder0PinA = 3;
+const char encoder0PinB = 2;
+const char plusPin = 5;
+const char minusPin = 7;
 
 volatile unsigned int encoder0Pos = 0;
 
 void setup() {
-
   pinMode(encoder0PinA, INPUT); 
   pinMode(encoder0PinB, INPUT); 
-
-// encoder pin on interrupt 0 (pin 2)
-
-  attachInterrupt(0, doEncoderA, CHANGE);
-
-// encoder pin on interrupt 1 (pin 3)
-
-  attachInterrupt(1, doEncoderB, CHANGE);  
-
-  Serial.begin (9600);
-
-}
-
-void loop(){
+  pinMode(plusPin, OUTPUT);
+  pinMode(minusPin, OUTPUT);
   
-  delay(100);
+  // encoder pin on interrupt 0 (pin 2)
+  attachInterrupt(0, doEncoderA, CHANGE);
+  // encoder pin on interrupt 1 (pin 3)
+  attachInterrupt(1, doEncoderB, CHANGE); 
+  
+  Serial.begin (9600);
 }
-void doEncoderA(){
 
+void loop(){  
+  // trying to get this to send a pulse out to the seven segment board
+  // but I don't think this will work. Maybe I can modify the interrupts?
+  if (encoder0Pos == 4){
+    digitalWrite(plusPin, HIGH);
+    digitalWrite(plusPin, LOW);
+}
+
+void doEncoderA(){
   // look for a low-to-high on channel A
   if (digitalRead(encoder0PinA) == HIGH) { 
-
     // check channel B to see which way encoder is turning
     if (digitalRead(encoder0PinB) == LOW) {  
       encoder0Pos = encoder0Pos + 1;         // CW
@@ -39,9 +44,7 @@ void doEncoderA(){
       encoder0Pos = encoder0Pos - 1;         // CCW
     }
   }
-
-  else   // must be a high-to-low edge on channel A                                       
-  { 
+  else {  // must be a high-to-low edge on channel A
     // check channel B to see which way encoder is turning  
     if (digitalRead(encoder0PinB) == HIGH) {   
       encoder0Pos = encoder0Pos + 1;          // CW
@@ -50,17 +53,15 @@ void doEncoderA(){
       encoder0Pos = encoder0Pos - 1;          // CCW
     }
   }
-  Serial.println (encoder0Pos, DEC);          
-  // use for debugging - remember to comment out
 
+  // use for debugging - remember to comment out
+  // Serial.println (encoder0Pos, DEC);          
 }
 
 void doEncoderB(){
-
   // look for a low-to-high on channel B
   if (digitalRead(encoder0PinB) == HIGH) {   
-
-   // check channel A to see which way encoder is turning
+    // check channel A to see which way encoder is turning
     if (digitalRead(encoder0PinA) == HIGH) {  
       encoder0Pos = encoder0Pos + 1;         // CW
     } 
@@ -68,9 +69,7 @@ void doEncoderB(){
       encoder0Pos = encoder0Pos - 1;         // CCW
     }
   }
-
   // Look for a high-to-low on channel B
-
   else { 
     // check channel B to see which way encoder is turning  
     if (digitalRead(encoder0PinA) == LOW) {   
@@ -80,5 +79,5 @@ void doEncoderB(){
       encoder0Pos = encoder0Pos - 1;          // CCW
     }
   }
-
 } 
+
