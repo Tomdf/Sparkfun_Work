d9392c9b9ebafadd5fb3762703a96597946e3ba5
Tom <tomdflock@gmail.com>
2013-11-16 07:49:42 -0700

    Working Counter Code, no 7Seg
    Got the interrupt working by working it out myself instead of copy /
    pasting. If the right encoder sees a falling edge and the left encoder
    is HIGH (blocked) it counts up. If the left encoder is LOW it counts
    down. Logic dummy!

--- Reel Counters/Inventory Reel Counter/v1/Arduino Code/Inventory_Reel_Counter_v4/Inventory_Reel_Counter_v4.ino
+++ Reel Counters/Inventory Reel Counter/v1/Arduino Code/Inventory_Reel_Counter_v4/Inventory_Reel_Counter_v4.ino
@@ -2,9 +2,16 @@
 #include <SoftwareSerial.h>
 SoftwareSerial Serial7Segment(8, 4); //RX pin, TX pin
 
+#define MODE_CMD 0x82
+#define MODE_DATA 0
+#define MODE_ANALOG 1
+#define MODE_COUNTER 2
+
+int cycles = 0;
+
 // setup pin configuration
-const char encoder0PinA = 3;
-const char encoder0PinB = 2;
+const char encoder0PinA = 2;
+const char encoder0PinB = 3;
 const char plusPin = 5;
 const char minusPin = 7;
 
@@ -19,24 +26,29 @@ void setup() {
   // encoder pin on interrupt 0 (pin 2)
   // interrupt when the first sensor transistions
   // from blocked to slot
-  attachInterrupt(0, doEncoderA, FALLING );
+  attachInterrupt(0, doEncoderA, FALLING);
   Serial.begin (9600);
+  Serial7Segment.begin(9600); //Talk to the Serial7Segment at 9600 bps
+  Serial7Segment.write('v'); //Reset the display - this forces the cursor to return to the beginning of the display
+  delay(10);
+  Serial7Segment.write(MODE_CMD); //Change the mode of the display
+  Serial7Segment.write(MODE_COUNTER); //Enter counter mode. Any pulse on SDI will go up. Any pulse on SDO will go down.
 }
 
 void loop(){  
-  Serial.print(encoder0Pos);
+  Serial.println(encoder0Pos);
 }
 
 void doEncoderA(){
-  if (digitalRead(encoder0PinB) == HIGH) {
-    digitalWrite(plusPin, HIGH);
-    digitalWrite(plusPin, LOW);
-    encoder0Pos += 1; 
-  }
-  else if (digitalRead(encoder0PinB) == LOW) {
-    digitalWrite(minusPin, HIGH);
-    digitalWrite(minusPin, LOW);
+  if (digitalRead(encoder0PinB) == LOW) {
+    //digitalWrite(plusPin, HIGH);
+    //digitalWrite(plusPin, LOW);
     encoder0Pos -= 1; 
+  }
+  else if (digitalRead(encoder0PinB) == HIGH) {
+    //digitalWrite(minusPin, HIGH);
+    //digitalWrite(minusPin, LOW);
+    encoder0Pos += 1; 
   }
 } 
 
